1aa0d44568d8dec07e3dac0fb889535ca1081f11
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-31 10:48:39 +0100

    transformed DB paste item to match interface type

--- src/components/ListOfPastes.tsx
+++ src/components/ListOfPastes.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { PasteItem } from "./PasteItemInterface";
+import { PasteItem, PasteItemDB } from "./PasteItemInterface";
 
 export function ListOfPastes(): JSX.Element {
     const [pastes, setPastes] = useState<PasteItem[]>([]);
@@ -10,28 +10,40 @@ export function ListOfPastes(): JSX.Element {
             const response = await axios.get(
                 "https://paste-bin-backend.onrender.com/pastes"
             );
-            const allPastes = response.data;
+            const allPastes = response.data.map((eachPaste: PasteItemDB) => {
+                const {
+                    id,
+                    title,
+                    description,
+                    creation_date: creationDate,
+                } = eachPaste;
+                return {
+                    id,
+                    title,
+                    description,
+                    creationDate,
+                };
+            });
+
             setPastes(allPastes);
         } catch (error) {
             console.error("Error:", error);
         }
     };
 
+    const renderEachPaste = pastes.map((paste) => (
+        <li key={paste.id}>
+            title: {paste.title}
+            date created: {paste.creationDate}
+            description: {paste.description}
+        </li>
+    ));
     useEffect(() => {
         getPastes();
     }, []);
     return (
         <>
-            <ol>
-                {" "}
-                {pastes.map((paste) => (
-                    <li key={paste.id}>
-                        title: {paste.title}
-                        date created: {paste.creationDate}
-                        description: {paste.description}
-                    </li>
-                ))}
-            </ol>
+            <ol> {renderEachPaste}</ol>
         </>
     );
 }
--- src/components/PasteItemInterface.tsx
+++ src/components/PasteItemInterface.tsx
@@ -9,3 +9,10 @@ export interface PostType {
     title: string;
     description: string;
 }
+
+export interface PasteItemDB {
+    id: number;
+    title: string;
+    description: string;
+    creation_date: string;
+}
