c955da46eefce65442021a17d7bcb4117bb7a8d4
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-31 11:55:20 +0100

    lifted getPastes function to App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,40 @@
 import { ListOfPastes } from "./ListOfPastes";
 import { InputPastes } from "./InputPastes";
 import "./App.css";
+import { PasteItemDB } from "./PasteItemInterface";
+import axios from "axios";
 
 function App() {
+    const getPastes = async () => {
+        try {
+            const response = await axios.get(
+                "https://paste-bin-backend.onrender.com/pastes"
+            );
+            const allPastes = response.data.map((eachPaste: PasteItemDB) => {
+                const {
+                    id,
+                    title,
+                    description,
+                    creation_date: creationDate,
+                } = eachPaste;
+                return {
+                    id,
+                    title,
+                    description,
+                    creationDate,
+                };
+            });
+
+            return allPastes;
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
     return (
         <div className="App">
             <InputPastes />
-            <ListOfPastes />
+            <ListOfPastes getPastes={getPastes} />
         </div>
     );
 }
